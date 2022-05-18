37f3a42a931bd011e6bc2330a38e4cbfb8d748b8
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-18 23:39:03 +0600

    create todo add api or todo get api

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT||5000;
+
+app.use(cors());
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ja3y1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const todoCollection = client.db("todo").collection("task");
+        app.post('/addTodo',async (req,res)=>{
+            const todoDetail = req.body;
+            const result = await todoCollection.insertOne(todoDetail);
+            res.send(result)
+        })
+        app.get('/todo',async(req,res)=>{
+            const allTodo = await todoCollection.find().toArray();
+            res.send(allTodo)
+        })
+    }
+    finally{
+        // client.close();
+    }
+}
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+  res.send('TODO APP IS RUNNING')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
