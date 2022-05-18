c4608294cc2a6b8ad5f44f7437422efc4d917fdb
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-19 02:34:49 +0600

    all api done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
@@ -11,27 +11,28 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ja3y1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const todoCollection = client.db("todo").collection("task");
-        app.post('/addTodo',async (req,res)=>{
+        app.post('/addTodo', async (req, res) => {
             const todoDetail = req.body;
             const result = await todoCollection.insertOne(todoDetail);
             res.send(result)
         })
-        app.get('/todo',async(req,res)=>{
-            const allTodo = await todoCollection.find().toArray();
+        app.get('/todo', async (req, res) => {
+            const email = req.query.email
+            const allTodo = await todoCollection.find({ email: email }).toArray();
             res.send(allTodo)
         })
-        app.delete('/todoDelete/:id',async(req,res)=>{
+        app.delete('/todoDelete/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await todoCollection.deleteOne(query);
             res.send(result)
         })
     }
-    finally{
+    finally {
         // client.close();
     }
 }
@@ -40,9 +41,9 @@ run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('TODO APP IS RUNNING')
+    res.send('TODO APP IS RUNNING')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
